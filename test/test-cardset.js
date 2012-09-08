0fb3926725693c296965b212bfb5268145d12a92
Mike Roberts <mike@kik.com>
2012-09-08 04:02:37 -0400

    Fixed parsing, added overloads to the constructor.

--- test/test-cardset.js
+++ test/test-cardset.js
@@ -12,5 +12,38 @@ vows.describe('CardSet').addBatch({
 
 			assert.deepEqual(cardset.cards, [Card('2s')]);
 		},
+		'Multiple cards': function() {
+			var cardset = new CardSet(Card('2s'), Card('3s'), Card('4s'));
+
+			assert.deepEqual(cardset.cards, [Card('2s'), Card('3s'), Card('4s')]);
+		},
+		'Array of cards': function() {
+			var cardset = new CardSet([Card('2s'), Card('3s'), Card('4s')]);
+
+			assert.deepEqual(cardset.cards, [Card('2s'), Card('3s'), Card('4s')]);
+		},
+		'Parsed list of cards': function() {
+			var cardset = new CardSet('2s', '3s', '4s');
+
+			assert.deepEqual(cardset.cards, [Card('2s'), Card('3s'), Card('4s')]);
+		},
+	},
+
+	'Parsing': {
+		'Single card': function() {
+			assert.deepEqual(CardSet.parse('2c').cards, [Card('2c')]);
+		},
+		'Multiple cards': function() {
+			assert.deepEqual(CardSet.parse('2c 3s').cards, [Card('2c'), Card('3s')]);
+		},
+	},
+
+	'Stringify': {
+		'Single card': function() {
+			assert.equal(CardSet.parse('5h').toString(), '5h');
+		},
+		'Multiple cards': function() {
+			assert.equal(CardSet.parse('Ad 6h').toString(), 'Ad 6h');
+		},
 	},
 }).run();
