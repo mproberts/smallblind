d7b1a86f4eb177e7d9074083098456a6fc3a66e6
Mike Roberts <mike@kik.com>
2012-09-08 06:15:02 -0400

    Added helper methods and subsets.

--- test/test-cardset.js
+++ test/test-cardset.js
@@ -68,5 +68,22 @@ vows.describe('CardSet').addBatch({
 		'Multiple card exclusion': function() {
 			assert.deepEqual(CardSet('2c 3d 4h 5s').add('2c 3d 4h 5s').cards, [Card('2c'), Card('3d'), Card('4h'), Card('5s')]);
 		}
-	}
+	},
+
+	'Subsets': {
+	},
+
+	'Helpers': {
+		'Full deck': function() {
+			var cards = '2c 3c 4c 5c 6c 7c 8c 9c 10c Jc Qc Kc Ac '
+			          + '2d 3d 4d 5d 6d 7d 8d 9d 10d Jd Qd Kd Ad '
+			          + '2h 3h 4h 5h 6h 7h 8h 9h 10h Jh Qh Kh Ah '
+			          + '2s 3s 4s 5s 6s 7s 8s 9s 10s Js Qs Ks As ';
+			var reference = new CardSet(cards);
+
+			var deck = CardSet.fullDeck();
+
+			assert.deepEqual(deck, reference);
+		},
+	},
 }).run();
