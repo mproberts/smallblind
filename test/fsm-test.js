5e580225ccd3e933b165085f0e652ee82d63befc
Mike Roberts <mike@kik.com>
2012-09-13 02:15:46 -0400

    Changed FSM design and added more eventing tests.

---
+++ test/fsm-test.js
@@ -0,0 +1,59 @@
+var vows   = require('vows'),
+    should = require('should'),
+    assert = require('assert');
+
+var fsm = require('../lib/fsm');
+
+vows.describe('fsm').addBatch({
+
+	'Setup': {
+		topic: fsm()
+			.transition('red', 'green').when('ready')
+			.transition('green', 'yellow').when('stop')
+			.transition('yellow', 'red').when('timeout')
+			.set('green'),
+
+		'States created': function(lights) {
+			assert.isTrue(lights.has('green'));
+			assert.isTrue(lights.has('yellow'));
+			assert.isTrue(lights.has('red'));
+		},
+		'Transition if exists': function(lights) {
+			lights.set('green');
+
+			assert.isTrue(lights.is('green'));
+
+			lights.advance('stop');
+			assert.isTrue(lights.is('yellow'));
+
+			lights.advance('timeout');
+			assert.isTrue(lights.is('red'));
+		}, 
+		'Do not transition if does not exist': function(lights) {
+			lights.set('green');
+
+			assert.isTrue(lights.is('green'));
+
+			lights.advance('ready');
+			assert.isTrue(lights.is('green'));
+
+			lights.advance('timeout');
+			assert.isTrue(lights.is('green'));
+		},
+		'State events sent': function(lights) {
+			var args = null;
+
+			// bind to the transition event
+			lights.on('yellow', function() {
+				args = Array.prototype.slice.apply(arguments);
+			});
+
+			lights.set('green');
+
+			lights.advance('stop', 1, 2, 3);
+
+			assert.isTrue(lights.is('yellow'));
+			assert.deepEqual(args, ['stop', 1, 2, 3]);
+		}
+	}
+}).export(module);
