f6dfa2cabab88d6d8cca2d54833e4d5d7384b2ec
Mike Roberts <mike@kik.com>
2012-09-14 22:35:54 -0400

    Fixed table issues, added deal tests.

--- test/table-test.js
+++ test/table-test.js
@@ -8,9 +8,74 @@ var CardSet = require('../lib/cardset'),
       Table = require('../lib/table');
 
 vows.describe('Game Flow').addBatch({
-	'Construction': {
-		'Basic': function() {
+	'Setup': {
+		topic: function() {
 			var table = new Table();
-		}
+
+			for (var i = 0; i < 3; ++i) {
+				table.addPlayer(i, 10000, i);
+			}
+
+			return table;
+		}(),
+
+		'Basic': function(table) {
+			table.reset();
+
+			assert.equal(0, table.community.count());
+			assert.equal(0, table.playersInHand[0].hand.count());
+			assert.equal(0, table.playersInHand[1].hand.count());
+			assert.equal(0, table.playersInHand[2].hand.count());
+		},
+
+		'Hands dealt': function(table) {
+			table.reset();
+
+			table.deal();
+
+			assert.equal(0, table.community.count());
+			assert.equal(2, table.playersInHand[0].hand.count());
+			assert.equal(2, table.playersInHand[1].hand.count());
+			assert.equal(2, table.playersInHand[2].hand.count());
+		},
+
+		'Flop dealt': function(table) {
+			table.reset();
+
+			table.deal();
+			table.deal();
+
+			assert.equal(3, table.community.count());
+			assert.equal(2, table.playersInHand[0].hand.count());
+			assert.equal(2, table.playersInHand[1].hand.count());
+			assert.equal(2, table.playersInHand[2].hand.count());
+		},
+
+		'Turn dealt': function(table) {
+			table.reset();
+
+			table.deal();
+			table.deal();
+			table.deal();
+
+			assert.equal(4, table.community.count());
+			assert.equal(2, table.playersInHand[0].hand.count());
+			assert.equal(2, table.playersInHand[1].hand.count());
+			assert.equal(2, table.playersInHand[2].hand.count());
+		},
+
+		'River dealt': function(table) {
+			table.reset();
+
+			table.deal();
+			table.deal();
+			table.deal();
+			table.deal();
+
+			assert.equal(5, table.community.count());
+			assert.equal(2, table.playersInHand[0].hand.count());
+			assert.equal(2, table.playersInHand[1].hand.count());
+			assert.equal(2, table.playersInHand[2].hand.count());
+		},
 	}
 }).export(module);
